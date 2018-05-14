21f896f557521238016e6da371e9e62209e2be2d
devAppleMango <devapplemango@gmail.com>
2018-05-15 00:18:08 +0900

    Remove unnecesarry

--- src/main/java/com/cyhee/rabit/oauth/AuthorizationServerConfig.java
+++ src/main/java/com/cyhee/rabit/oauth/AuthorizationServerConfig.java
@@ -1,7 +1,5 @@
 package com.cyhee.rabit.oauth;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,9 +17,6 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-
-	private TestPasswordEncoder bCryptPasswordEncoder = new TestPasswordEncoder();
-
 	@Autowired
 	private JdbcClientDetailsService clientDetailsService;
 
@@ -31,30 +26,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private JwtAccessTokenConverter tokenConverter;
 
-	private static class TestPasswordEncoder implements PasswordEncoder {
-
-		@Override
-		public String encode(CharSequence rawPassword) {
-			return rawPassword.toString();
-		}
-
-		@Override
-		public boolean matches(CharSequence rawPassword, String encodedPassword) {
-			return rawPassword.toString().equals(encodedPassword);
-		}
-
-	}
-
-	@Autowired
-	private AuthenticationManager authenticationManager;
-
-	/**
-	 * Defines the security constraints on the token endpoints /oauth/token_key and /oauth/check_token
-	 * Client credentials are required to access the endpoints
-	 *
-	 * @param oauthServer
-	 * @throws Exception
-	 */
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
 		PasswordEncoder encoder = new BCryptPasswordEncoder(12);
@@ -65,22 +36,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		;
 	}
 
-	/**
-	 * Defines the authorization and token endpoints and the token services
-	 *
-	 * @param endpoints
-	 * @throws Exception
-	 */
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		super.configure(endpoints);
 		endpoints
-		//
-		//		// Which authenticationManager should be used for the password grant
-		//		// If not provided, ResourceOwnerPasswordTokenGranter is not configured
-		//		.authenticationManager(authenticationManager)
-		//
-		//		// Use JwtTokenStore and our jwtAccessTokenConverter
 		.tokenStore(tokenStore)
 		.accessTokenConverter(tokenConverter)
 		;
